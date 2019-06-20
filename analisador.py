6dbc071e7eb658db77bc69162298c55a9add3b24
otaviosumi <otaviosumi@hotmail.com>
2019-06-20 14:45:03 +0000

    some advance...

--- analisador.py
+++ analisador.py
@@ -6,16 +6,16 @@ reserved_word = [ "program",  "var" , "if" , "else", "then", "while", "do", "for
 is_in_comment = False;
 
 symbol_msg = {
+	">=": " - simbolo de maior igual que",
+	"<=": " - simbolo de menor igual que",
 	":": " - simbolo de dois pontos", 
-	"=": " - simbolo de igual",
 	";": " - simbolo de ponto e virgula",
 	",": " - simbolo de virgula",
 	"(": " - simbolo de abre parenteses",
 	")": " - simbolo de fecha parenteses",
 	">": " - simbolo de maior que",
-	">=": " - simbolo de maior igual que",
 	"<": " - simbolo de menor que",
-	"<=": " - simbolo de menor igual que",
+	"=": " - simbolo de igual",
 	"+": " - simbolo de mais",
 	"-": " - simbolo de menos",
 	"/": " - simbolo de divisao",
@@ -30,23 +30,52 @@ identifier_msg = {
 	"digit;": " - identifier is digit"
 }
 
-#
+def fragment_sub_tokens (in_word): #tira expressoes
+	for sub_sy in symbol_msg:
+		symb_pos = in_word.find(sub_sy)
+		if symb_pos == 0:  #eh apenas o simbolo
+			if len(sub_sy) == len(in_word):
+				return [ 0, in_word, 0]
+			else:
+				return [ 0, sub_sy, in_word[symb_pos+len(sub_sy): len(in_word)]]
+	for sub_sy in symbol_msg:
+		symb_pos = in_word.find(sub_sy)		
+		if symb_pos > 0:
+			return [ in_word[0:symb_pos], sub_sy, in_word[symb_pos+len(sub_sy): len(in_word)]]
+		# el
+	for sub_sy in symbol_msg:
+		symb_pos = in_word.find(sub_sy)
+		if symb_pos == -1:  #nao achou nada
+			return [ in_word, 0, 0]
+
 
 #funcoes de suporte
 def check_token (in_word):
 	global is_in_comment
+	tk_list = fragment_sub_tokens(in_word)
 	if (not is_in_comment):
+		if in_word == "\n":
+			return
 		if (in_word[0] == "{"):
 			is_in_comment = True
+		elif tk_list[1] != 0 and tk_list[2] != 0:
+			#procura no dicionario de simbolos
+			if(tk_list[0] != 0):
+				if (len(tk_list[0]) > 15):
+					print ("Token muito grande")
+				else:
+					print (tk_list[0] + ' - id')
+			if(tk_list[1] != 0):
+				print (tk_list[1] + symbol_msg[tk_list[1]])
+			if(tk_list[2] != 0):
+				if (len(tk_list[2]) > 15):
+					print ("Token muito grande")
+				elif (tk_list[2] != '') :
+					print (tk_list[2] + ' - id')
 		elif (in_word in reserved_word): #procura da lista de palavras reservadas
 			print (in_word + ' - Palavra reservada')
-		elif (len(in_word) > 15):
-			print ("Token muito grande")
 		elif (in_word in identifier_msg): #identificadores
 			print (in_word + identifier_msg[in_word])
-		else:
-			#procura no dicionario de simbolos
-			print(in_word)
 	elif (in_word[-1] == "}"):
 		is_in_comment = False
 
